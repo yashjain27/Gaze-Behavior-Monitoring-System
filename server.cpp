fbaba821ca3402d08a827871fd50fc51cd14c8c4
Yash Jain <yashjaindx@gmail.com>
2020-05-04 18:18:19 -0400

    updated server and tb

--- server.cpp
+++ server.cpp
@@ -1,8 +1,13 @@
 #include "systemc.h"
 
 SC_MODULE(server){
-	sc_in<bool> begin1, begin2, begin3, end1, end2, end3, incoming1, incoming2, incoming3;
-	sc_out<bool> free, outgoing1, outgoing2, outgoing3;
+	sc_in<bool> begin1{"begin1"}, begin2{"begin2"}, begin3{"begin3"};
+	sc_in<bool> end1{"end1"}, end2{"end2"}, end3{"end3"};
+	sc_in<bool> incoming1{"incoming1"}, incoming2{"incoming2"}, incoming3{"incoming3"};
+	sc_out<bool> free{"free"};
+	sc_out<bool> outgoing1{"outgoing1"};
+	sc_out<bool> outgoing2{"outgoing2"};
+	sc_out<bool> outgoing3{"outgoing3"};
 
 	void monitor(){
 		free = true;
@@ -11,7 +16,9 @@ SC_MODULE(server){
 		outgoing3 = false;
 
 		while(true){
-			wait(incoming1 | incoming2 | incoming3);
+			cout << "Inside this loop" << endl;
+			wait(incoming1.posedge_event() | incoming2.posedge_event() | incoming3.posedge_event());
+			cout << "After first wait" << endl;
 
 			//Mobile 1
 			if(incoming1 == true){
@@ -21,8 +28,10 @@ SC_MODULE(server){
 				//Now that the server has freed up, proceed
 				outgoing1 = true;
 				free = false;
-				wait(begin1);
-				wait(end1);
+				wait(begin1.posedge_event());
+				cout << "Begin1 @" << sc_simulation_time() << endl;
+				wait(end1.posedge_event());
+				cout << "End1 @" << sc_simulation_time() << endl;
 				free = true;
 			}
 
@@ -34,8 +43,10 @@ SC_MODULE(server){
 				//Now that the server has freed up, proceed
 				outgoing2 = true;
 				free = false;
-				wait(begin2);
-				wait(end2);
+				wait(begin2.posedge_event());
+				cout << "Begin2 @" << sc_simulation_time() << endl;
+				wait(end2.posedge_event());
+				cout << "End2 @" << sc_simulation_time() << endl;
 				free = true;
 			}
 
@@ -47,18 +58,21 @@ SC_MODULE(server){
 				//Now that the server has freed up, proceed
 				outgoing3 = true;
 				free = false;
-				wait(begin3);
-				wait(end3);
+				wait(begin3.posedge_event());
+				cout << "Begin3 @" << sc_simulation_time() << endl;
+				wait(end3.posedge_event());
+				cout << "End3 @" << sc_simulation_time() << endl;
 				free = true;
 			}
 		}
 	}
 
 	SC_CTOR(server){
-		outgoing1 = false;
-		outgoing2 = false;
-		outgoing3 = false;
+		//outgoing1 = false;
+		//outgoing2 = false;
+		//outgoing3 = false;'
+		cout << "Started" << endl;
 		SC_THREAD(monitor);
-		sensitive << incoming1.pos() << incoming2.pos() << incoming3.pos();
+		//sensitive << incoming1.pos() << incoming2.pos() << incoming3.pos();
 	}
 };
---
+++ tb_server.cpp
@@ -0,0 +1,70 @@
+#include "systemc.h"
+#include "server.cpp"
+
+int sc_main(int argc, char* argv[]){
+  //Inputs
+  sc_signal<bool> begin1;
+  sc_signal<bool> begin2;
+  sc_signal<bool> begin3;
+  sc_signal<bool> end1;
+  sc_signal<bool> end2;
+  sc_signal<bool> end3;
+  sc_signal<bool> incoming1;
+  sc_signal<bool> incoming2;
+  sc_signal<bool> incoming3;
+  sc_signal<bool> outgoing1;
+  sc_signal<bool> outgoing2;
+  sc_signal<bool> outgoing3;
+  sc_signal<bool> free;
+
+  server srvr("server");
+  srvr.begin1(begin1);
+  srvr.begin2(begin2);
+  srvr.begin3(begin3);
+  srvr.end1(end1);
+  srvr.end2(end2);
+  srvr.end3(end3);
+  srvr.incoming1(incoming1);
+  srvr.incoming2(incoming2);
+  srvr.incoming3(incoming3);
+  srvr.outgoing1(outgoing1);
+  srvr.outgoing2(outgoing2);
+  srvr.outgoing3(outgoing3);
+  srvr.free(free);
+
+  //Deprecate
+  sc_core::sc_report_handler::set_actions( "/IEEE_Std_1666/deprecated",
+                                           sc_core::SC_DO_NOTHING );
+
+  incoming1 = 0;
+  incoming2 = 0;
+  incoming3 = 0;
+  begin1 = 0;
+  begin2 = 0;
+  begin3 = 0;
+  end1 = 0;
+  end2 = 0;
+  end3 = 0;
+
+  sc_start(10, SC_MS);
+
+  incoming1 = 1;
+  incoming2 = 1;
+  incoming3 = 1;
+
+  sc_start(10, SC_MS);
+
+  begin1 = 1;
+
+  sc_start(10, SC_MS);
+
+  begin1 = 0;
+
+  sc_start(10, SC_MS);
+
+  end1 = 1;
+
+  sc_start(10, SC_MS);
+
+  end1 = 0;
+}
